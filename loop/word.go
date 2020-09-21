77dfda27cb42d1188c9587189b8ee53279841040
Adirak Kaewmahing <adirak@gmail.com>
2020-09-21 17:12:07 +0700

    decrease loop to 5

--- loop/word.go
+++ loop/word.go
@@ -2,7 +2,7 @@ package loop
 
 // GenWord to gen word
 func GenWord() string {
-	count := 100
+	count := 5
 	result := ""
 	for i := 0; i < count; i++ {
 		result += "Hello world WTF!!!!!!!!!!!....\n"
