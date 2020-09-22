b903787ceae6623f5d8e3dc88fcdb3efc6308cb0
Adirak Kaewmahing <adirak@gmail.com>
2020-09-22 15:06:52 +0700

    remove !!!

--- loop/word.go
+++ loop/word.go
@@ -2,10 +2,10 @@ package loop
 
 // GenWord to gen word
 func GenWord() string {
-	count := 5
+	count := 1000
 	result := ""
 	for i := 0; i < count; i++ {
-		result += "Hello world WTF!!!!!!!!!!!....\n"
+		result += "  Hello world!....\n"
 	}
 
 	return result
